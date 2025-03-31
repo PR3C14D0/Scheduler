0c2a36be7f29e61776bfd1422b52c369fc054587
PR3C14D0 <aarongarpre@gmail.com>
2025-03-31 20:56:55 +0200

    Upload Source files

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout << "Welcome to Scheduler Injector!" << std::endl;
+    std::cout << "By: PR3C14D0" << std::endl;
+    std::cout << "Github: https://github.com/PR3C14D0" << std::endl;
+
+    return 0;
+}
