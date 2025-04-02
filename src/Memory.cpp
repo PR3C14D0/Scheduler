a95f750a4becbfdb02cd47d7f99eb4eab70984f3
PR3C14D0 <aarongarpre@gmail.com>
2025-04-02 15:41:59 +0200

    Started with the relay JMP instruction

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,6 +1,21 @@
 #include "Memory.h"
 
 LPVOID Memory::CreateHook(LPVOID lpSrc, LPVOID lpDst, UINT nMangledBytes, LPVOID& lpGateway) {
+	/*
+		Relay: ABS_JMP Size: 
+			6 bytes instruction 
+					+
+			8 bytes address
+			Total: 14 bytes
+
+		Gateway: 
+			Mangled bytes (X bytes)
+					+
+			JMP32 (5 bytes)
+
+		The ABS JMP will jump to our hook function, and then, call the gateway.
+	*/
+
 	LPVOID lpLoc = (char*)lpSrc - 0x2000;
 	LPVOID lpRelay = nullptr;
 
@@ -13,11 +28,19 @@ LPVOID Memory::CreateHook(LPVOID lpSrc, LPVOID lpDst, UINT nMangledBytes, LPVOID
 
 	LPVOID lpAddr = lpRelay;
 
-	LPVOID lpMangledBytes = VirtualAlloc(nullptr, nMangledBytes, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	LPVOID lpMangledBytes = VirtualAlloc(nullptr, nMangledBytes, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 	memcpy(lpMangledBytes, lpSrc, nMangledBytes);
 	VirtualProtect(lpMangledBytes, nMangledBytes, PAGE_READONLY, nullptr);
 	
 	std::cout << "Mangled bytes address: 0x" << std::hex << (DWORD_PTR)lpMangledBytes << std::endl;
 
-	return 0x00;
+	const char ABS_JMP[] = {
+		0xFF, 0x25, 0x00, 0x00, 0x00, 0x00
+	};
+
+	UINT nJmpSize = sizeof(ABS_JMP);
+	
+
+
+	return lpRelay;
 }
